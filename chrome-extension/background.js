d7ed809d596c6eba446f48c34d3b28ada33037c6
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-04-05 23:08:10 +0900

    [Clean] インデント: Space -> Tab

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -2,23 +2,23 @@
 var ID_TRANSLATE_BY_GOOGLE = "translate-by-google";
 
 function createContextMenus() {
-    chrome.contextMenus.create({
-        title: "選択文字をgoogle翻訳",
-        contexts: ["selection"],
-        id: ID_TRANSLATE_BY_GOOGLE
-    });
+	chrome.contextMenus.create({
+		title: "選択文字をgoogle翻訳",
+		contexts: ["selection"],
+		id: ID_TRANSLATE_BY_GOOGLE
+	});
 }
 
 chrome.runtime.onInstalled.addListener(createContextMenus);
 chrome.runtime.onStartup.addListener(createContextMenus);
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-    if (info.menuItemId === ID_TRANSLATE_BY_GOOGLE) {
-        var word = info.selectionText;
-        openGoogleTranslatePage(word, {
-            tab: tab
-        });
-    }
+	if (info.menuItemId === ID_TRANSLATE_BY_GOOGLE) {
+		var word = info.selectionText;
+		openGoogleTranslatePage(word, {
+			tab: tab
+		});
+	}
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
@@ -37,31 +37,31 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 });
 
 function openGoogleTranslatePage(word, options){
-    if (!options) options = {};
-    var openSameWindow = options.openSameWindow;
+	if (!options) options = {};
+	var openSameWindow = options.openSameWindow;
 	// 文単位で改行する
-    word = word.replace(/([.]"?) +(?=[A-Z])/g, "$1\n\n");
-    var url = "https://translate.google.co.jp/?hl=ja&q=" + encodeURIComponent(word);
-    chrome.windows.get(chrome.windows.WINDOW_ID_CURRENT, function(currentWindowInfo) {
-        if (!openSameWindow && currentWindowInfo.state === "normal") {
-            var currentLeft = currentWindowInfo.left;
-            var currentWidth = currentWindowInfo.width;
-            var currentHeight = currentWindowInfo.height;
-            chrome.windows.create({
-                url: url,
-                top: 0,
-                height: currentHeight,
-                left: currentLeft === 0 ? currentWidth : 0,
-                width: currentWidth
-            });
-        } else {
-            var createProperties = {
-                url: url
-            };
-            if (options.tab && options.tab.id !== chrome.tabs.TAB_ID_NONE) {
-                createProperties.openerTabId = options.tab.id;
-            }
-            chrome.tabs.create(createProperties);
-        }
-    });
+	word = word.replace(/([.]"?) +(?=[A-Z])/g, "$1\n\n");
+	var url = "https://translate.google.co.jp/?hl=ja&q=" + encodeURIComponent(word);
+	chrome.windows.get(chrome.windows.WINDOW_ID_CURRENT, function(currentWindowInfo) {
+		if (!openSameWindow && currentWindowInfo.state === "normal") {
+			var currentLeft = currentWindowInfo.left;
+			var currentWidth = currentWindowInfo.width;
+			var currentHeight = currentWindowInfo.height;
+			chrome.windows.create({
+				url: url,
+				top: 0,
+				height: currentHeight,
+				left: currentLeft === 0 ? currentWidth : 0,
+				width: currentWidth
+			});
+		} else {
+			var createProperties = {
+				url: url
+			};
+			if (options.tab && options.tab.id !== chrome.tabs.TAB_ID_NONE) {
+				createProperties.openerTabId = options.tab.id;
+			}
+			chrome.tabs.create(createProperties);
+		}
+	});
 }
